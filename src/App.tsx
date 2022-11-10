c051b36d7d7a752b3ee16742ca48922352385942
Falllbackodin <yimsjoseph@knights.ucf.edu>
2022-11-10 15:30:32 -0500

    Adds a route for invalid links

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,18 @@ import { Register } from "./pages/Register";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Landing } from "./pages/Landing";
 import { About } from "./pages/AboutUs";
+import { NotFound } from "./pages/NotFound";
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Landing />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/about" element={<About />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
