a1c41c61d6725a8911df34b9e2339f8ac7241e96
Falllbackodin <yimsjoseph@knights.ucf.edu>
2023-01-09 14:04:00 -0500

    makes progress on pagination

--- src/pages/Competitions.tsx
+++ src/pages/Competitions.tsx
@@ -7,29 +7,33 @@ export function Competitions() {
       <div
         style={{
           border: "2px solid red",
-          padding: "10px 10px 10px 10px",
-          paddingBottom: "0px",
+          padding: "10px",
           margin: "1% 5%",
           position: "absolute",
           width: "75vw",
-          height: "75vh",
+          height: "80vh",
           overflow: "hidden",
         }}
       >
         <Grid
           justify="center"
           grow
-          style={{ position: "relative", height: "100%", width: "100%" }}
+          style={{
+            position: "relative",
+            height: "100%",
+            width: "100%",
+            margin: "0 auto",
+          }}
         >
-          <Grid.Col style={{ border: "1px solid blue" }} span={3}>
+          <Grid.Col style={{ border: "1px solid blue" }} span={5}>
             <p>1</p>
             <p>testyo</p>
             <p>testyo</p>
 
-            <Pagination total={10} />
+            <Pagination total={10} size="sm" />
           </Grid.Col>
           <MediaQuery smallerThan="md" styles={{ display: "none" }}>
-            <Grid.Col style={{ border: "1px solid green" }} span={9}>
+            <Grid.Col style={{ border: "1px solid green" }} span={7}>
               2
             </Grid.Col>
           </MediaQuery>
