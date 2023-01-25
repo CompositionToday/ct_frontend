363462bf65b5fa6231706e95f9500d00dc83fb69
Justin King <jlkingogie@knights.ucf.edu>
2023-01-25 04:24:15 -0500

    main mobile expand bar as wide as screen

--- src/pages/JustinNavbar.tsx
+++ src/pages/JustinNavbar.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { Menu, Button, Text } from '@mantine/core';
-import { IconSettings, IconSearch, IconPhoto, IconMessageCircle, IconTrash, IconArrowsLeftRight } from '@tabler/icons';
+//import { IconSettings, IconSearch, IconPhoto, IconMessageCircle, IconTrash, IconArrowsLeftRight } from '@tabler/icons'; icons that im not using
 
 export function JustinNavbar() {
   return (
-    <Menu shadow="md" width={200}>
+    <Menu shadow="xl"
+    radius={"lg"}
+    width={200} >
       <Menu.Target>
-        <Button>Menu</Button>
+        <Button style={{ width: "100%" , height: "200px" }}>Menu</Button>
       </Menu.Target>
 
-      <Menu.Dropdown>
-        <Menu.Item >Jobs</Menu.Item>
+      <Menu.Dropdown >
+        <Menu.Item  >Jobs</Menu.Item>
         <Menu.Item >Competitions</Menu.Item>
         <Menu.Item >Festivals</Menu.Item>
         <Menu.Item >Concerts</Menu.Item>
