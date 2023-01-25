bf566ddf38bc7933eead807381c40648c3908d8c
Justin King <jlkingogie@knights.ucf.edu>
2023-01-25 03:53:44 -0500

    created the mobile navbar mantine menu

--- src/pages/JustinNavbar.tsx
+++ src/pages/JustinNavbar.tsx
@@ -1,5 +1,23 @@
 import React from "react";
+import { Menu, Button, Text } from '@mantine/core';
+import { IconSettings, IconSearch, IconPhoto, IconMessageCircle, IconTrash, IconArrowsLeftRight } from '@tabler/icons';
 
 export function JustinNavbar() {
-  return <div>JustinNavbar</div>;
+  return (
+    <Menu shadow="md" width={200}>
+      <Menu.Target>
+        <Button>Menu</Button>
+      </Menu.Target>
+
+      <Menu.Dropdown>
+        <Menu.Item >Jobs</Menu.Item>
+        <Menu.Item >Competitions</Menu.Item>
+        <Menu.Item >Festivals</Menu.Item>
+        <Menu.Item >Concerts</Menu.Item>
+        <Menu.Divider />
+        <Menu.Item >Login</Menu.Item>
+        <Menu.Item >Register</Menu.Item>
+      </Menu.Dropdown>
+    </Menu>
+  );
 }
