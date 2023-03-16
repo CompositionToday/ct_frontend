50c8dc88e30ffed236064279b273b1bf95f93268
Falllbackodin <yimsjoseph@knights.ucf.edu>
2023-03-16 19:09:49 -0400

    Starts to implement scuba mask for easter egg

--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -1,7 +1,12 @@
+import { useState } from "react";
 import { createStyles, Container, Title, Text, Image } from "@mantine/core";
 import { Teeter } from "../animations/AnimateOnHover";
+import { IconScubaMask } from "@tabler/icons";
+import ScubaMask from "../../images/scuba-mask.png";
+import Eyes from "../../images/eyes.png";
 
 const heroLogo = require("../../images/HeroLogo.png");
+const scubaLogo = require("../../images/scuba-mask.png");
 
 const useStyles = createStyles((theme) => ({
   inner: {
@@ -87,6 +92,40 @@ const useStyles = createStyles((theme) => ({
 
 export function Hero() {
   const { classes } = useStyles();
+  const [rotateDegree, setRotateDegree] = useState(0);
+  const [mouseX, setMouseX] = useState(0);
+  const [mouseY, setMouseY] = useState(0);
+
+  function angle(cx: number, cy: number, ex: number, ey: number) {
+    const dy = ey - cy;
+    const dx = ex - cx;
+    const rad = Math.atan2(dy, dx);
+    const deg = (rad * 180) / Math.PI;
+    return deg;
+  }
+
+  const anchor = document.getElementById("anchor");
+  const rect = anchor?.getBoundingClientRect();
+
+  const anchorX = rect?.left! + rect?.width! / 2;
+  const anchorY = rect?.top! + rect?.height! / 2;
+  document.addEventListener("mousemove", (e) => {
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+    setMouseX(mouseX);
+    setMouseY(mouseY);
+
+    const angleDeg = angle(mouseX, mouseY, anchorX, anchorY);
+    console.log(angleDeg);
+
+    const eyes = document.querySelectorAll<HTMLElement>(`.eye`);
+    eyes.forEach((eye) => {
+      eye.style.transform! = `rotate(${90 + angleDeg}deg)`;
+    });
+    // setRotateDegree(angleDeg);
+    console.log("here");
+  });
+
   return (
     <div>
       <Container className={classes.container}>
@@ -118,10 +157,63 @@ export function Hero() {
               An online hub for musicians to find jobs, competitions, festivals,
               and concerts.
             </Text>
+            <Text>
+              MouseX: {mouseX} <br />
+              MouseY: {mouseY}
+            </Text>
           </div>
-          <Teeter rotation={5} timing={120}>
+          {/* <Teeter rotation={5} timing={120}>
             <Image src={String(heroLogo)} className={classes.image} />
-          </Teeter>
+          </Teeter> */}
+          <div className={classes.image} style={{ position: "relative" }}>
+            <IconScubaMask
+              size="lg"
+              color="#359fec"
+              className={classes.image}
+              id="anchor"
+            />
+            {/* <img src={Eyes} style={{ position: "absolute" }} /> */}
+            <div
+              style={{
+                position: "absolute",
+                padding: "0px",
+                paddingTop: "10px",
+                borderRadius: "10px",
+                top: "40%",
+                left: "23%",
+                transform: `rotate(${90 + rotateDegree}deg)`,
+              }}
+              className="eye"
+            >
+              <div
+                style={{
+                  background: "black",
+                  borderRadius: "10px",
+                  padding: "4px",
+                }}
+              ></div>
+            </div>
+            <div
+              style={{
+                position: "absolute",
+                padding: "0px",
+                paddingTop: "10px",
+                borderRadius: "10px",
+                top: "40%",
+                right: "39%",
+                transform: `rotate(${90 + rotateDegree}deg)`,
+              }}
+              className="eye"
+            >
+              <div
+                style={{
+                  background: "black",
+                  borderRadius: "10px",
+                  padding: "4px",
+                }}
+              ></div>
+            </div>
+          </div>
         </div>
       </Container>
     </div>
