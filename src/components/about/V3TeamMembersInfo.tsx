b3b41306dc909e0eff438f09f79e5f9e6ad0d722
JPacheco35 <jo208746@ucf.edu>
2024-03-27 20:38:33 -0400

    V3 Team Member Description Update

--- src/components/about/V3TeamMembersInfo.tsx
+++ src/components/about/V3TeamMembersInfo.tsx
@@ -3,27 +3,27 @@ export const V3TeamMembersInfo = {
         {
             avatar: "michaelV3",
             name: "Michael Ritcher",
-            role: "Michael was the Project Manager and a Lead Content Curator for the project.",
+            role: "Michael was the Project Manager and Content Curator for the project.",
         },
         {
             avatar: "johnV3",
             name: "John Pacheco",
-            role: "John was the Lead Front-End Developer and worked on SEO for the project.",
+            role: "John was the Front-End Developer for the project.",
         },
         {
             avatar: "justinV3",
             name: "Justin Gonzalez",
-            role: "Justin was the Lead API Developer and Database Manager for the project",
+            role: "Justin was the Backend/API Developer and Database Manager for the project",
         },
         {
             avatar: "treyV3",
             name: "Trey Zaccour",
-            role: "Trey was the Lead Mobile Application Developer for the project.",
+            role: "Trey was the Mobile Application Developer for the project.",
         },
         {
             avatar: "valentinoV3",
             name: "Valentino Rozzi",
-            role: "Valentino was the Head SEO Specialist for the project",
+            role: "Valentino was the SEO Specialist for the project",
         },
     ],
 };
