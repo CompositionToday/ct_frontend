3ec6994b4f14be36948d728291812bb345cfd86d
JPacheco35 <jo208746@ucf.edu>
2024-04-03 11:49:09 -0400

    UI Updates to the Twitter(X) and Facebook Share Buttons

--- src/components/opportunity/shareButtons.jsx
+++ src/components/opportunity/shareButtons.jsx
@@ -1,8 +1,10 @@
-import { Button } from "@mantine/core";
+import {Button, Tooltip, useMantineColorScheme} from "@mantine/core";
 import React from "react";
 import { FacebookShareButton, TwitterShareButton, FacebookIcon, TwitterIcon } from "react-share";
 import { OpportunityItem } from "./OpportunityHelper";
 import infoicon from "../../../src/images/info-square.png";
+import { IconBrandFacebook, IconBrandX } from '@tabler/icons-react';
+
 const ShareButtons = ({value, userUID}) => {
   const shareUrl = "https://compositiontoday.net/#/compositions"; // Get the current URL
   let eventDetails = "test";
@@ -11,17 +13,30 @@ const ShareButtons = ({value, userUID}) => {
   return (
     <div style={{display: "grid", gridAutoFlow: "column", gridColumnGap: "7px"}}>
       <span style={{width:"30px", margin: "0 0 20 0"}}>
-      <FacebookShareButton url={shareUrl} hashtag={eventDetails} subject={eventDetails} quote={"Testing"}>
-        <FacebookIcon width={"25px"} viewBox="0 40 64 64"/>
-      </FacebookShareButton>
+          <Tooltip label="Share on Facebook">
+              <FacebookShareButton
+                  url={shareUrl}
+                  title={eventDetails}
+              >
+                {/*<FacebookIcon width={"25px"} viewBox="0 40 64 64"/>*/}
+                <IconBrandFacebook style={{marginTop:"2px"}} stroke={2} color={"#1DA1F2"}/>
+            </FacebookShareButton>
+          </Tooltip>
       </span>
+
       <span style={{margin: "auto"}}>
-      <TwitterShareButton
-        url={shareUrl}
-        title={eventDetails}
-      >
-        <TwitterIcon width={"25px"} viewBox="0 40 64 64"/>
-      </TwitterShareButton>
+          <Tooltip label="Share on X">
+              <TwitterShareButton
+                url={shareUrl}
+                title={eventDetails}
+              >
+            {/*<TwitterIcon width={"25px"} viewBox="0 40 64 64"/>*/}
+            <IconBrandX stroke={2}
+                color={useMantineColorScheme().colorScheme === 'dark' ? "#FFFFFF" : "#222222"}
+                style={{marginTop:"2px"}}
+            />
+            </TwitterShareButton>
+          </Tooltip>
       </span>
     </div>
   );
