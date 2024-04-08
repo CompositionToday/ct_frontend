29aa5f9e6a6737dc4f22a3c7457d0b9c685336a9
Michael Richter <70779298+RichterMJ@users.noreply.github.com>
2024-04-08 15:09:55 -0400

    adding user survey

--- src/components/footer/FooterInfo.tsx
+++ src/components/footer/FooterInfo.tsx
@@ -7,6 +7,10 @@ export const footerInfo = {
           label: "Meet the Team",
           link: "/about",
         },
+        {
+          label: "Website Feedback",
+          link: "https://docs.google.com/forms/d/e/1FAIpQLSfiwwk4MlmhSU6Vn10zcmi-Nd-mxFu_iiybGlzBseE8wsz56g/viewform?usp=sf_link",
+        },
       ],
     },
   ],
